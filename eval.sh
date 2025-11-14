121bf005e43c3b724cd48ac23849c2d64c8965ea
QinkaiXu <qinkxu@gmail.com>
2025-11-14 14:44:23 +0800

    release: release original code

---
+++ eval.sh
@@ -0,0 +1,17 @@
+export CUDA_VISIBLE_DEVICES=0
+export MODEL_PATH=meta-llama/Llama-2-7b-hf
+export DATASET_PATH=c4
+
+python main.py $MODEL_PATH $DATASET_PATH\
+    --nsamples=1024 \
+    --val_size=128 \
+    --num_codebooks=1 \
+    --nbits_per_codebook=16 \
+    --in_group_size=8 \
+    --relative_mse_tolerance=0.01 \
+    --finetune_batch_size=32 \
+    --finetune_max_epochs=10 \
+    --finetune_early_stop=3 \
+    --finetune_keep_best \
+    --local_batch_size=1 \
+    --offload_activations
--- main.py
+++ main.py
@@ -294,6 +294,11 @@ def quantize_aq(model: PreTrainedModel, data: Sequence, val_data: Optional[Seque
             print("PREPARING TO FINETUNE")
             print(layer)
             layer = layer.to(dtype=torch.float32)
+            print(f"inps type: {type(inps)}")
+            print(f"inps length: {len(inps)}")
+            print(f"inps[0] type: {type(inps[0])}")
+            print(f"inps[0] shape: {inps[0].shape}")
+            exit()
             with using_tf32(enabled=True):
                 layer = finetune_groupwise(
                     layer=layer,
